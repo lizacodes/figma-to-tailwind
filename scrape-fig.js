6a4fb2e307d2af14579e86fc38adea1826195481
liza <liza@noogn.net>
2018-08-04 23:34:23 +1000

    init commit

---
+++ scrape-fig.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const util = require('util');
+const request = require('request-promise');
+const snakeCase = require('lodash.snakecase');
+
+const AUTH_TOKEN = '<INSERT_AUTH_TOKEN_HERE>';
+const API_ROOT = 'https://api.figma.com';
+
+const fileKey = '<INSERT_FILE_KEY_HERE>';
+
+const getFile = async () => {
+  try {
+    // Get Figma JSON
+    const response = await request.get(`${API_ROOT}/v1/files/${fileKey}`, {
+      headers: {
+        'X-FIGMA-TOKEN': AUTH_TOKEN
+      }
+    });
+
+    return JSON.parse(response);
+  } catch(e) {
+    console.log(e);
+  }
+
+}
+
+const parseColours = (group, figmaStyles, colours = {}) => {
+  if (group.styles && group.styles.fill && figmaStyles[group.styles.fill]) {
+    const { r, g, b, a } = group.fills[0].color;
+
+    colours[snakeCase(figmaStyles[group.styles.fill].name)] = `rgba(${Math.floor(r*256)},${Math.round(g*256)},${Math.round(b*256)},${a})`;
+  }
+
+  if (group.children) {
+    for (let index = 0; index < group.children.length; index++) {
+      const child = group.children[index];
+      parseColours(child, figmaStyles, colours);
+    }
+  }
+
+  return colours;
+}
+
+getFile().then((file) => {
+  const colours = parseColours(file.document, file.styles);
+
+  fs.writeFileSync('./colours.json', JSON.stringify(colours, null, 2) , 'utf-8');
+}).catch((e) => {
+  console.log(e);
+});
