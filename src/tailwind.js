f46e3ca18f55452bbf08dd95a799daa92fd0c509
Liza Nguyen <liza.nguyen@ratemyagent.com>
2018-08-31 13:22:13 +1000

    Font fam

--- src/tailwind.js
+++ src/tailwind.js
@@ -112,36 +112,15 @@ module.exports = {
 
   fonts: {
     'sans': [
-      'system-ui',
-      'BlinkMacSystemFont',
-      '-apple-system',
-      'Segoe UI',
-      'Roboto',
-      'Oxygen',
-      'Ubuntu',
-      'Cantarell',
-      'Fira Sans',
-      'Droid Sans',
+      'Open Sans',
       'Helvetica Neue',
       'sans-serif',
     ],
     'serif': [
-      'Constantia',
-      'Lucida Bright',
-      'Lucidabright',
-      'Lucida Serif',
-      'Lucida',
-      'DejaVu Serif',
-      'Bitstream Vera Serif',
-      'Liberation Serif',
-      'Georgia',
+      'Garamond serif',
       'serif',
     ],
     'mono': [
-      'Menlo',
-      'Monaco',
-      'Consolas',
-      'Liberation Mono',
       'Courier New',
       'monospace',
     ]
